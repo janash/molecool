035c005e438d87254b0821d95d5c1ee0c27acac2
Jessica Nash <janash@vt.edu>
2025-07-29 15:42:32 -0400

    fixing imports

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,7 @@
 from .measure import calculate_distance, calculate_angle
 from .molecule import build_bond_list
 from .visualize import draw_molecule, bond_histogram
-from . import io
+from .io import open_pdb, open_xyz, write_xyz
 
 
 from ._version import __version__
